f743834eca6a8e156fe49ea89de320e4091e0974
Lauren Revere <laurenrevere@cos.io>
2018-01-31 15:03:51 -0500

    Add User class

--- pythosf/client.py
+++ pythosf/client.py
@@ -392,3 +392,27 @@ class Provider(Folder):
     pass
 
 
+class User(APIDetail):
+    def __init__(self, session, id=None, self_link=None, data=None):
+        super().__init__(session=session, data=data)
+        if not data:
+            self.id = id
+            self.type = 'users'
+            self.links = None
+            self.meta = None
+            self.self_link = self_link
+
+    def get(self, query_parameters=None, token=None):
+        url = '/v2/users/me/'
+        if self.self_link:
+            url = self.self_link
+        elif self.links:
+            url = self.links.self
+        elif self.id:
+            url = '/v2/users/{}/'.format(self.id)
+
+        response = self.session.get(url=url, query_parameters=query_parameters, token=token)
+        if response:
+            self._update(response=response)
+        else:
+            raise ValueError("No url or id to get. Set the id or self_link then try to get.")
